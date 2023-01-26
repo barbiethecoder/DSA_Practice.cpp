4c3fda94ddd53f35e51bc5ff15b912745803bab0
Tripti <114797483+barbiethecoder@users.noreply.github.com>
2023-01-26 20:29:53 +0530

    Create Basic2.cpp

---
+++ Basic2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(){
+    int b;
+    long int g;
+    char ch;
+    float u;
+    double l;
+    cin>>b>>g>>ch>>u>>l;
+    cout<<b<<"\n"<<g<<"\n"<<ch<<"\n"<<u<<"\n"<<l;
+    return 0;
+}
